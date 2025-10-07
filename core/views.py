9cb1b7b50c84c640dd2da006c105b96b148c91ee
renan <renan.silveira@macrosul.com>
2025-10-07 10:31:37 -0300

    juntando as páginas e adicionando verificação da url com implementação do data-theme

--- core/views.py
+++ core/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_macrosul(request):
-    return render(request,'home_macrosul.html')
+    return render(request,'home.html')
 
 def home_medicina(request):
-    return render(request,'home_linhas.html')
+    return render(request,'home.html')
 
 def home_veterinaria(request):
-    return render(request,'home_linhas.html')
+    return render(request,'home.html')
 
 def home_esportes(request):
-    return render(request,'home_linhas.html')
+    return render(request,'home.html')
 
 def home_cosmeticos(request):
-    return render(request,'home_linhas.html')
+    return render(request,'home.html')
